b668e9c4f0c350750d71d1b29a00aae23b1fd9cb
ujkkju117 <ujkkju117@outlook.com>
2025-12-30 12:18:44 +1030

    1.3.2 release. DependencyUnityPlugins is now used and implement logic for unpatching

--- AssetToolTipPlugin/AssetToolTip.cs
+++ AssetToolTipPlugin/AssetToolTip.cs
@@ -1,30 +1,76 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using PluginUtilities;
+using System.Reflection;
 using UnityEngine;
 
 namespace AssetToolTip
 {
     [BepInPlugin(Guid, Name, Version)]
     [BepInDependency(SetInjectionFlag.Guid)]
-    public sealed class AssetToolTip : BaseUnityPlugin
+    public sealed class AssetToolTip : DependencyUnityPlugin
     {
         // constants
         public const string Guid = "org.hollofox.plugins.AssetToolTip";
         private const string Version = "0.0.0.0";
         private const string Name = "Asset Tool Tip";
 
+        private Harmony harmony;
+
         /// <summary>
         /// Awake plugin
         /// </summary>
-        void Awake()
+        protected override void OnAwake()
         {
-            Logger.LogInfo("In Awake for AssetToolTip");
-            Debug.Log("AssetToolTip Plug-in loaded");
+            Logger.LogDebug("In Awake for AssetToolTip");
             ModdingTales.ModdingUtils.AddPluginToMenuList(this, "HolloFoxes'");
 
-            var harmony = new Harmony(Guid);
+            harmony = new Harmony(Guid);
             harmony.PatchAll();
+
+
+            // Add MouseTextOnHover to existing slot items
+            Object[] slotItems = Resources.FindObjectsOfTypeAll(typeof(UI_AssetBrowserSlotItem));
+
+            FieldInfo field = typeof(UI_AssetBrowserSlotItem)
+                .GetField("_gridData", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (Object item in slotItems)
+            {
+                UI_AssetBrowserSlotItem slotItem = item as UI_AssetBrowserSlotItem;
+
+                if (slotItem == null)
+                    continue;
+
+                MouseTextOnHover mouseText = slotItem.GetComponent<MouseTextOnHover>();
+                if (mouseText != null)
+                    continue;
+
+                UI_AssetLibraryCategory.DataItem data = field.GetValue(slotItem) as UI_AssetLibraryCategory.DataItem;
+                slotItem.gameObject.AddComponent<MouseTextOnHover>().mouseHoverText = data.Name;
+            }
+        }
+
+        protected override void OnDestroyed()
+        {
+            Logger.LogDebug("In OnDestroyed for AssetToolTip");
+            harmony.UnpatchSelf();
+            Logger.LogDebug("Unpatched all Harmony patches for AssetToolTip");
+
+            // Remove MouseTextOnHover from existing slot items
+            Object[] slotItems = Resources.FindObjectsOfTypeAll(typeof(UI_AssetBrowserSlotItem));
+            foreach (Object item in slotItems)
+            {
+                UI_AssetBrowserSlotItem slotItem = item as UI_AssetBrowserSlotItem;
+                if (slotItem != null)
+                {
+                    MouseTextOnHover mouseText = slotItem.GetComponent<MouseTextOnHover>();
+                    if (mouseText != null)
+                    {
+                        Object.Destroy(mouseText);
+                    }
+                }
+            }
         }
     }
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+- 1.3.2: DependencyUnityPlugins is now used and implement logic for unpatching
 - 1.3.1: Fix deployment pipeline, update documentations, recompiled to keep up to date
 - 1.3.0: Simplify patch and fix tooltip. Removed Config Manager dependency
 - 1.2.6: Re-build for Attachment point update
